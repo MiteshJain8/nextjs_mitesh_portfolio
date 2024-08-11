6b3f81bf7643301547a09e361fab4b373cf72c97
Mitesh Jain <miteshjain2808@gmail.com>
2024-08-11 13:26:02 +0530

    updt2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import profilegif from "../assets/profilegif.gif"
 const Hero = () => {
     return (
         <div className="py-24 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1947_35%,#8F5C55_60%,#DBAF6E_80%)]">
-            <div className="absolute rounded-[50%] w-[3000px] h-[1300px] top-[530px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_80%,#2B1947)]"></div>
+            <div className="absolute rounded-[50%] w-[2200px] md:w-[3000px] h-[1300px] top-[570px] sm:top-[700px] md:top-[750px] lg:top-[800px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_80%,#2B1947)]"></div>
             <div className="relative">
                 <div className="font-bold lg:text-8xl md:text-7xl text-5xl text-center">
                     <h1 className="text-[#98B4CE]">Hi, my name is</h1>
